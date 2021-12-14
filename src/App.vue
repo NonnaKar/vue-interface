dc078a90b2e75943372dabacb786247e0a7dd981
nonna <ukr.nonna@gmail.com>
2021-12-13 21:26:44 -0500

    looping through data with directives

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h4>{{ title }}</h4>
+    <FontAwesomeIcon icon="plus" />Add Appointment
+    <div v-for="(item, i) in appointments" :key="i">
+      <h4>{{ item.petName }}</h4>
+      <p>{{ item.aptNotes }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Appointment List",
+      appointments: [
+        {
+          petName: "Spot",
+          petOwner: "Constance Smith",
+          aptDate: "2017-07-24 08:30",
+          aptNotes: "This German Shepherd is having some back pain",
+        },
+        {
+          petName: "Goldie",
+          petOwner: "Barot Bellingham",
+          aptDate: "2017-07-22 15:50",
+          aptNotes: "This Goldfish has some weird spots in the belly",
+        },
+        {
+          petName: "Mitten",
+          petOwner: "Hillary Goldwyn",
+          aptDate: "2017-07-21 9:15",
+          aptNotes: "Cat has excessive hairballs",
+        },
+        {
+          petName: "Buffy",
+          petOwner: "Hassum Harrod",
+          aptDate: "2017-07-20 15:30",
+          aptNotes:
+            "This Chihuahua has not eaten for three days and is lethargic",
+        },
+      ],
+    };
+  },
+  components: { FontAwesomeIcon },
+};
 </script>
 
 <style></style>
